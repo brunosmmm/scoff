252975b5e15cb75a19bbdb97eddef22f7e1fa862
Bruno Morais <brunosmmm@gmail.com>
2019-04-19 13:59:14 -0400

    implement faster way to access children in AST objects

--- scoff/parsers/__init__.py
+++ scoff/parsers/__init__.py
@@ -8,7 +8,23 @@ class ScoffASTObject:
 
     def __init__(self, **kwargs):
         """Initialize."""
-        if "SCOFF_META" in kwargs:
-            self.SCOFF_META = kwargs.pop("SCOFF_META")
+        self.__visitable_children = {}
+        self.__not_visitable_children = {}
         for name, value in kwargs.items():
+            # ignore special names
+            if not name.startswith("_") and name != "parent":
+                self.__visitable_children[name] = value
+            else:
+                self.__not_visitable_children[name] = value
             setattr(self, name, value)
+
+        self.__visitable_children_names = list(self.__visitable_children.keys())
+
+    @property
+    def visitable_children(self):
+        """Get visitable children."""
+        return self.__visitable_children
+
+    def __dir__(self):
+        """Get dir."""
+        return self.__visitable_children_names

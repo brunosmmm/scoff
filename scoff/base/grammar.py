dab57e386e594efb9fdcae9be6551ff8f16763ee
Bruno <brunosmmm@gmail.com>
2021-03-10 12:55:48 -0500

    Rename, document

--- scoff/base/grammar.py
+++ scoff/base/grammar.py
@@ -4,15 +4,25 @@ from typing import Tuple, Any
 from textx.metamodel import metamodel_from_file
 
 
-def _parse_text(text: str, grammar_file: str) -> Tuple[str, Any]:
-    """Parse text directly."""
+def parse_text(text: str, grammar_file: str) -> Tuple[str, Any]:
+    """Parse text directly.
+
+    :param text: Text to be parsed
+    :param grammar_file: Path to grammar file
+    :return: Tuple (text, AST)
+    """
     meta = metamodel_from_file(grammar_file)
     decl = meta.model_from_str(text)
     return (text, decl)
 
 
-def _parse_file(filename: str, grammar_file: str) -> Tuple[str, str]:
-    """Parse some source."""
+def parse_file(filename: str, grammar_file: str) -> Tuple[str, str]:
+    """Parse a source file.
+
+    :param filename: Path to file
+    :param grammar_file: Path to grammar file
+    :return: Tuple(text, AST)
+    """
     f = open(filename, "r")
     text = "".join(f.readlines())
     f.close()

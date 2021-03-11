3ae8a5e87b693d543c24d0f98fb778d0faa4e278
Bruno <brunosmmm@gmail.com>
2021-03-11 13:29:09 -0500

    Test code generation correctness

--- tests.sh
+++ tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+mkdir -p tmp
 
-python $(which coverage) run --source=scoff ./examples/StateMachine/sm_visit.py --dump examples/StateMachine/miss_grant_controller.sm > /dev/null
+
+echo "INFO: performing test coverage analysis"
+# pass 1
+python $(which coverage) run --source=scoff ./examples/StateMachine/sm_visit.py --dump examples/StateMachine/miss_grant_controller.sm > tmp/generated.sm 2> tmp/model.txt
 mv .coverage .coverage.sm
 python $(which coverage) run --source=scoff ./examples/StateMachine/gen_classes.py > /dev/null
 mv .coverage .coverage.gen
@@ -10,3 +14,17 @@ coverage_report_options='-i --omit=usage/*,tests/*,venv/*,tools/*'",${virtualenv
 coverage combine
 coverage html $coverage_report_options
 coverage report $coverage_report_options
+
+# test parsing generated code
+echo "INFO: testing code generation"
+./examples/StateMachine/sm_visit.py tmp/generated.sm 2> tmp/generated_model.txt
+diff tmp/generated_model.txt tmp/model.txt > /dev/null
+if [[ $? != 0 ]]; then
+    # diff failed
+    echo "ERROR: generated model is not identical to base model"
+    rm -rf tmp
+    exit 1
+fi
+
+rm -rf tmp
+echo "INFO: done"

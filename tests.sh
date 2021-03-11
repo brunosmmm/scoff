4214811215f4cdf7e4c5404fe5e584a5e109b8f9
Bruno <brunosmmm@gmail.com>
2021-03-11 14:02:24 -0500

    Update tests

--- tests.sh
+++ tests.sh
@@ -9,11 +9,19 @@ python $(which coverage) run --source=scoff ./examples/StateMachine/sm_visit.py 
 mv .coverage .coverage.sm
 python $(which coverage) run --source=scoff ./examples/StateMachine/gen_classes.py > /dev/null
 mv .coverage .coverage.gen
+
+# test checking examples
+python $(which coverage) run --source=scoff ./examples/StateMachine/sm_check.py examples/StateMachine/assets/err1.sm > /dev/null
+mv .coverage .coverage.err1
+python $(which coverage) run --source=scoff ./examples/StateMachine/sm_check.py examples/StateMachine/assets/err2.sm > /dev/null
+mv .coverage .coverage.err2
+
 virtualenv_root=$(poetry env info -p)
 coverage_report_options='-i --omit=usage/*,tests/*,venv/*,tools/*'",${virtualenv_root}"'/*'
 coverage combine
 coverage html $coverage_report_options
 coverage report $coverage_report_options
+rm -rf .coverage*
 
 # test parsing generated code
 echo "INFO: testing code generation"

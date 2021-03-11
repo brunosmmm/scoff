2ad4c18e418f66988b4cadd23cf6cc917ff34a18
Bruno <brunosmmm@gmail.com>
2021-03-11 11:08:48 -0500

    Pass AST classes in

--- scoff/base/grammar.py
+++ scoff/base/grammar.py
@@ -1,24 +1,28 @@
 """Parse grammars."""
 
-from typing import Tuple
+from typing import Tuple, List, Type
 from textx.metamodel import metamodel_from_file
 
 from scoff.ast import ScoffASTObject
 
 
-def parse_text(text: str, grammar_file: str) -> Tuple[str, ScoffASTObject]:
+def parse_text(
+    text: str, grammar_file: str, classes: List[Type]
+) -> Tuple[str, ScoffASTObject]:
     """Parse text directly.
 
     :param text: Text to be parsed
     :param grammar_file: Path to grammar file
     :return: Tuple (text, AST)
     """
-    meta = metamodel_from_file(grammar_file)
+    meta = metamodel_from_file(grammar_file, classes=classes)
     decl = meta.model_from_str(text)
     return (text, decl)
 
 
-def parse_file(filename: str, grammar_file: str) -> Tuple[str, ScoffASTObject]:
+def parse_file(
+    filename: str, grammar_file: str, classes: List[Type]
+) -> Tuple[str, ScoffASTObject]:
     """Parse a source file.
 
     :param filename: Path to file
@@ -29,4 +33,4 @@ def parse_file(filename: str, grammar_file: str) -> Tuple[str, ScoffASTObject]:
     text = "".join(f.readlines())
     f.close()
 
-    return parse_text(text, grammar_file)
+    return parse_text(text, grammar_file, classes)
